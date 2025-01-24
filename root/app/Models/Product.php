3c9df67d28f0d5bf85af2a47cbb01a8bcfd1a9d5
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2025-01-24 19:38:25 +0400

    i created relationships

--- root/app/Models/Product.php
+++ root/app/Models/Product.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'category_products';
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_products');
+    }
+
+    public function vendor(): BelongsTo
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 }
--- root/app/Models/Vendor.php
+++ root/app/Models/Vendor.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Vendor extends Model
 {
     use HasFactory;
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
